c507952f8a8b1e2ca4f756f52dbf86225de0aa8a
Al Johri <al.johri@gmail.com>
2014-12-24 07:45:32 -0500

    finished 2.4, use delete not free, switch func arg
    switch to passing reference to linked list head so that the head of the list can be deleted / changed

--- chapter2.cpp
+++ chapter2.cpp
@@ -11,7 +11,7 @@ class Node {
 		Node() : next(NULL) {};
 		Node(T mydata) : data(mydata), next(NULL) {};
 		Node(T mydata, Node* mynext) : data(mydata), next(mynext) {};
-		~Node();
+		~Node() {};
 };
 
 template<class T>
@@ -23,8 +23,8 @@ void removeDuplicates(Node<T>* head) {
 	while ((prev = cur, cur = cur->next)) {
 		if (map[cur->data] == true) {
 			prev->next = cur->next;
-			free(cur);
-			cur = prev->next;
+			delete cur;
+			cur = prev;
 		}
 		else {
 			map[cur->data] = true;
@@ -43,7 +43,7 @@ void removeDuplicates2(Node<T>* head) {
 		while ((prev = cur, cur = cur->next)) {
 			if (cur->data == dupCheck->data) {
 				prev->next = cur->next;
-				free(cur);
+				delete cur;
 			}
 		}
 	} while((dupCheck = dupCheck->next));
@@ -78,20 +78,48 @@ Node<int>* createLinkedList() {
 }
 
 template<class T>
-void removeNode(Node<T>* head, T item) {
+void removeNode(Node<T>*& head, T item) {
 	Node<T>* cur = head;
 	if (head->data == item) {
 		head = head->next;
-		free(cur);
+		delete cur;
 	}
 	Node<T>* prev;
 	while ((prev = cur, cur = cur->next)) {
 		if (cur->data == item) {
 			prev->next = cur->next;
-			free(cur);
-			cur = prev->next;
+			delete cur;
+			cur = prev;
+			return; // deletes only one instance of item
 		}
 	}
+	return;
+}
+
+template<class T>
+void appendItem(Node<T>*&head, Node<T>*&tail, Node<T>*cur) {
+	if (head == NULL) { head = cur; tail = cur; }
+	else { tail->next = cur; tail = cur; }
+}
+
+template<class T>
+void pivotList(Node<T>*&head, T pivot) {
+	Node<T>* headLeft = NULL; Node<T>* tailLeft = NULL;
+	Node<T>* headPivot = NULL; Node<T>* tailPivot = NULL;
+	Node<T>* headRight = NULL; Node<T>* tailRight = NULL;
+
+	Node<T>* cur = head;
+	do {
+		if (cur->data == pivot) appendItem(headPivot, tailPivot, cur);
+		else if (cur->data > pivot) appendItem(headRight, tailRight, cur);
+		else if (cur->data < pivot) appendItem(headLeft, tailLeft, cur);
+	} while ((cur = cur->next));
+
+	tailLeft->next = headPivot;
+	tailPivot->next = headRight;
+	head = headLeft;
+
+	return;
 }
 
 int main() {
@@ -117,9 +145,27 @@ int main() {
 	Node<int>* head4 = createLinkedList();
 	printAll(head4);
 	removeNode(head4, 2);
+	printAll(head4);
 	removeNode(head4, 5);
+	printAll(head4);
 	removeNode(head4, 5);
+	printAll(head4);
 	removeNode(head4, 4);
 	printAll(head4);
+	removeNode(head4, 1);
+	printAll(head4);
+	cout << endl;
+
+	Node<int>* g = new Node<int>(6);
+	Node<int>* f = new Node<int>(1, g);
+	Node<int>* e = new Node<int>(4, f);
+	Node<int>* d = new Node<int>(7, e);
+	Node<int>* c = new Node<int>(3, d);
+	Node<int>* b = new Node<int>(5, c);
+	Node<int>* head5 = new Node<int>(2, b);
+	printAll(head5);
+	pivotList(head5, 3);
+	printAll(head5);
+	cout << endl;
 }
 
