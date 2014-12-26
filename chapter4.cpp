a0508d66786e486caff2dec2f3c0e39c585b3ba5
Al Johri <al.johri@gmail.com>
2014-12-26 10:48:37 -0500

    start chapter 4, implement simple binary tree

---
+++ chapter4.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+template<class T>
+class BinaryTree;
+
+template<class T>
+class Node {
+	friend class BinaryTree<T>;
+	public:
+		Node* left;
+		Node* right;
+		T data;
+		const int id;
+		Node() : id(count++), left(NULL), right(NULL) {};
+		Node(T mydata) : id(count++), data(mydata), left(NULL), right(NULL) {};
+		~Node() {};
+	protected:
+		static int count;
+};
+
+template<class T>
+int Node<T>::count = 0;
+
+template<class T>
+class BinaryTree {
+    public:
+        BinaryTree() : root(NULL) {};
+        ~BinaryTree() {};
+
+        void insert(T key);
+        Node<T> *search(T key);
+        void destroy_tree();
+
+        void visulizeTree();        
+    private:
+        void destroy_tree(Node<T>* leaf);
+        void insert(T key, Node<T>* leaf);
+        Node<T> *search(T key, Node<T>* leaf);
+
+        Node<T> *root;
+};
+
+template<class T>
+void BinaryTree<T>::insert(T key, Node<T>* leaf) {
+	if (key < leaf->data) {
+		if (leaf->left != NULL) insert(key, leaf->left);
+		else leaf->left = new Node<T>(key);
+	}
+	else {
+		if (leaf->right != NULL) insert(key, leaf->right);
+		else leaf->right = new Node<T>(key);
+	}
+}
+
+template<class T>
+void BinaryTree<T>::insert(T key) {
+	if (this->root != NULL) {
+		insert(key, this->root);
+	}
+	else {
+		this->root = new Node<T>(key);
+	}
+}
+
+template<class T>
+BinaryTree<T>* createRandomTree(int num) {
+	BinaryTree<T>* bt = new BinaryTree<T>();
+	srand(time(NULL));
+	for (int i = 0; i < num; ++i) {
+		int random = rand() % 100;
+		bt->insert(random);
+	}
+	return bt;
+}
+
+template<class T>
+void BinaryTree<T>::visulizeTree() {
+
+	stringstream ss1, ss2;
+
+	stack<Node<T>*> nodes;
+	unordered_map<int,T> map;
+	nodes.push(this->root);
+
+	while(nodes.empty() == false) {
+		Node<T>* cur = nodes.top(); nodes.pop();
+		map[cur->id] = cur->data;
+		if (cur->left) ss2 << "\tnode" << cur->id << ":left -> " << "node" << cur->left->id << ";" << endl;
+		if (cur->right) ss2 << "\tnode" << cur->id << ":right -> " << "node" << cur->right->id << ";" << endl;
+		if (cur->right) nodes.push(cur->right);
+        if (cur->left) nodes.push(cur->left);
+	}
+
+	for (int i = 0; i < this->root->count; ++i) {
+		ss1 << "\tnode" << i << "[label=\"{" << i << "(" << map[i] << ")" << "|{<left>|<right>}}\"];" << endl;
+	}
+
+	ofstream myfile;
+	myfile.open ("sample.dot");
+	myfile << "digraph G{" << endl;
+	myfile << "\tnode[shape=record];" << endl;
+	myfile << ss1.str() << ss2.str();
+	myfile << "}" << endl;
+	myfile.close();
+	return;
+}
+
+int main() {
+	BinaryTree<int>* bt = createRandomTree<int>(100);
+	bt->visulizeTree();
+}
+
+// dot -Tjpeg sample.dot -o sample.jpg
