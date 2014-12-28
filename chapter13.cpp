621a2aa7a9a53db3ebdf37ae311e09e2ec7444ca
Al Johri <al.johri@gmail.com>
2014-12-28 01:44:19 -0500

    list questions in chapter 13 and 14

---
+++ chapter13.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// 13.1 Write a method to print the last K lines of an input file using C++.
+
+// 13.2 Compare and contrast a hash table and an STL map. How is a hash table implemented?
+// If the number of inputs is small, which data structure options can be used
+// instead of a hash table?
+
+// 13.3 How do virtual functions work in C++?
+
+// 13.4 What is the difference between deep copy and shallow copy? Explain how you would
+// use each.
+
+// 13.5 What is the significance of the keyword "volatile" in C?
+
+// 13.6 Why does a destructor in base class need to be declared virtual?
+
+// 13.7 Write a method that takes a pointer to a Node structure as a parameter and returns
+// a complete copy of the passed in data structure. The Node data structure contains
+// two pointers to other Nodes.
+
+// 13.8 Write a smart pointer class. A smart pointer is a data type, usually implemented with
+// templates, that simulates a pointer while also providing automatic garbage collection.
+// It automatically counts the number of references to a SmartPointer<T*>
+// object and frees the object of type T when the reference count hits zero.
+
+// 13.9 Write an aligned malloc and free function that supports allocating memory such
+// that the memory address returned is divisible by a specific power of two.
+
+// 13.10 Write a function in C called my2DAlloc which allocates a two-dimensional array.
+// Minimize the number of calls to malloc and make sure that the memory is accessible
+// by the notation arr[i][j].
+
+int main() {
+	return 0;
+}
--- chapter14.java
+++ chapter14.java
@@ -1,4 +1,20 @@
 public class chapter14 {
+
+	// 14.1 In terms of inheritance, what is the effect of keeping a constructor private?
+
+	// 14.2 In Java, does the finally block get executed if we insert a return statement inside the
+	// try block of a try-catch-finally?
+
+	// 14.3 What is the difference between final, finally, and finalize?
+
+	// 14.4 Explain the difference between templates in C++ and generics in Java.
+
+	// 14.5 Explain what object reflection is in Java and why it is useful.
+
+	// 14.6 Implement a Circular Array class that supports an array-like data structure which
+	// can be efficiently rotated. The class should use a generic type, and should support
+	// iteration via the standard for (Obj o : circuLarArray) notation.
+
     public static void main(String args[]) {
         System.out.println("Hello World!");
     }
