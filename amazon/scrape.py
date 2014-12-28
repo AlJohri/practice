0082a0ca6617c6f681c1c6fb4219fd8d954e0842
Al Johri <al.johri@gmail.com>
2014-12-28 09:36:03 -0500

    update amazon readme

--- amazon/scrape.py
+++ amazon/scrape.py
@@ -1,4 +1,5 @@
 import requests, lxml.html, re, csv
+from dateutil.parser import parse
 
 posts = []
 
@@ -12,18 +13,25 @@ for i in range(12, 0, -1):
     for item in doc.cssselect(".post-info"):
         title = item.cssselect('.post-title a')[0].text.replace("Amazon interview Experience", "").replace("Amazon Interview Experience", "").replace("Amazon Interview Questions", "").replace("Amazon Interview", "").replace(" | ", "").strip()
         url = item.cssselect('.post-title a')[0].get('href')
-        date = item.cssselect('.post-date')[0]
+        title_url = "[%s](%s)" % (title, url)
+        date = item.cssselect('.post-date')[0].text
+        try:
+            pydate = parse(date)
+        except:
+            pass
         matches = re.findall(regex, title)
         if matches:
-            posts.append(("set" + str(matches[0]).zfill(3), title, url, date.text))
+            posts.append(("set" + str(matches[0]).zfill(3), title_url, date, pydate))
         else:
-            posts.append(("other" + str(other_counter).zfill(3), title, url, date.text))
+            posts.append(("other" + str(other_counter).zfill(3), title_url, date, pydate))
             other_counter += 1
         print(posts[-1])
 
+posts = sorted(posts, key=lambda x: x[3], reverse=True)
+
 f = open("posts.csv", "w")
 writer = csv.writer(f)
-writer.writerow(["Title", "URL", "Date"])
+writer.writerow(["Title", "Date"])
 for post in posts:
-    writer.writerow(post[1:])
+    writer.writerow(post[1:3])
 f.close()
