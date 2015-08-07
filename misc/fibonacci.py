d59993e6f74d328fbd0cd9fce6d0852ee8a24e4d
Al Johri <al.johri@gmail.com>
2015-08-06 22:20:16 -0400

    modify fibonacci to use lru cache

--- misc/fibonacci.py
+++ misc/fibonacci.py
@@ -1,10 +1,17 @@
 import timeit
 
+from functools import lru_cache
+
 def fibonacci1(n):
     if n <= 1: return n
     return fibonacci1(n - 1) + fibonacci1(n - 2)
 
+@lru_cache(maxsize=None)
 def fibonacci2(n):
+    if n <= 1: return n
+    return fibonacci2(n-1) + fibonacci2(n-2)
+
+def fibonacci3(n):
     a, b = 0, 1
     for i in range(0, n):
         a, b = b, a + b
@@ -12,6 +19,10 @@ def fibonacci2(n):
 
 t1 = timeit.Timer("fibonacci1(10)", "from __main__ import fibonacci1")
 t2 = timeit.Timer("fibonacci2(10)", "from __main__ import fibonacci2")
+t3 = timeit.Timer("fibonacci3(10)", "from __main__ import fibonacci3")
 
-print t1.timeit(1000);
-print t2.timeit(1000);
+print(t1.timeit(1000))
+print(t2.timeit(1000))
+print(t3.timeit(1000))
+
+print(fibonacci2.cache_info())
