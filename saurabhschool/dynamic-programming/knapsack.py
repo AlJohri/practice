7f292a3a8c3eff41cc1a11ec91f0694947f9bc28
Al Johri <al.johri@gmail.com>
2015-08-06 22:14:28 -0400

    update knapsack problem

--- saurabhschool/dynamic-programming/knapsack.py
+++ saurabhschool/dynamic-programming/knapsack.py
@@ -7,9 +7,23 @@ max_weight = 13
 from functools import lru_cache
 
 @lru_cache(maxsize=None)
+def knapsack_val(items, max_weight):
+	potential_knapsacks = [value + knapsack_val(items[i+1:], max_weight-weight)
+		for i, (value, weight) in enumerate(items) if weight <= max_weight]
+	return max(potential_knapsacks, default=0) if len(items) > 0 and max_weight > 0 else 0
+
+print(knapsack_val(items, max_weight))
+print(knapsack_val.cache_info())
+
+print("--------------------------------------------------------------")
+
+sum_by_value = lambda items: sum(value for value, weight in items)
+
+@lru_cache(maxsize=None)
 def knapsack(items, max_weight):
-	return max(value + knapsack(items[i+1:], max_weight-weight) if weight <= max_weight else 0
-			for i, (value, weight) in enumerate(items)) if len(items) > 0 and max_weight > 0 else 0
+	potential_knapsacks = [((value, weight),) + knapsack(items[i+1:], max_weight-weight)
+		for i, (value, weight) in enumerate(items) if weight <= max_weight]
+	return max(potential_knapsacks, key=sum_by_value, default=()) if len(items) > 0 and max_weight > 0 else ()
 
 print(knapsack(items, max_weight))
 print(knapsack.cache_info())
