3b8811f401904774847ca213e37d1d774d72404c
Al Johri <al.johri@gmail.com>
2015-09-10 19:51:19 -0400

    fix spacing interviewcake problem2

--- interviewcake/problem2.py
+++ interviewcake/problem2.py
@@ -1,7 +1,10 @@
 # https://www.interviewcake.com/question/product-of-other-numbers
-
-# You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
-# Write a function get_products_of_all_ints_except_at_index() that takes an array of integers and returns an array of the products.
+#
+# You have an array of integers, and for each index you want to find the
+# product of every integer except the integer at that index.
+#
+# Write a function get_products_of_all_ints_except_at_index() that takes an
+# array of integers and returns an array of the products.
 
 # For example, given:
 
